23f802c62f89307c11f80cbd304f95bc5805a6dc
ylehilds <lehi_alcantara@byu.edu>
2022-10-06 10:55:30 -0600

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Lehi Alcantara on 10/6/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
