7102f73751ee9e0bc858d317eae329470a3582db
ylehilds <lehi_alcantara@byu.edu>
2022-10-11 12:25:20 -0600

    finished with Scrumdinger

---
+++ Scrumdinger/Models/History.swift
@@ -0,0 +1,25 @@
+//
+//  History.swift
+//  Scrumdinger
+//
+//  Created by Lehi Alcantara on 10/10/22.
+//
+
+import Foundation
+
+struct History: Identifiable, Codable {
+    let id: UUID
+    let date: Date
+    var attendees: [DailyScrum.Attendee]
+    var lengthInMinutes: Int
+    var transcript: String?
+    
+    init(id: UUID = UUID(), date: Date = Date(), attendees: [DailyScrum.Attendee], lengthInMinutes: Int = 5, transcript: String? = nil) {
+            self.id = id
+            self.date = date
+            self.attendees = attendees
+            self.lengthInMinutes = lengthInMinutes
+            self.transcript = transcript
+        }
+}
+
